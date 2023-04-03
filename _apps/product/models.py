9193a27bbf31dd47fabfba7972e432b0b1c4e260
Endang Ismaya <endang.ismaya@gmail.com>
2023-04-04 00:57:24 +0700

    add models: Category, Brand, Product

--- _apps/product/models.py
+++ _apps/product/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=100)
+    parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.parent}"
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, default="")
+    is_digital = models.BooleanField(default=True)
+    brand = models.ForeignKey(
+        Brand, on_delete=models.CASCADE, related_name="product_brand"
+    )
+    category = TreeForeignKey(
+        "Category",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="product_category",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
